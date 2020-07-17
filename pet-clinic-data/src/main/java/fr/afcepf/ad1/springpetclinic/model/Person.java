a8c865e7f308c51de2c9e3d06155239e2cef15c9
Benjamin Bonche <benjamin.bonche@gmail.com>
2020-07-17 10:27:21 +0200

    Added entities and Lombok annotations. Closes #3

---
+++ pet-clinic-data/src/main/java/fr/afcepf/ad1/springpetclinic/model/Person.java
@@ -0,0 +1,12 @@
+package fr.afcepf.ad1.springpetclinic.model;
+
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public abstract class Person extends BaseEntity {
+    private String firstName;
+    private String lastName;
+
+}
---
+++ pet-clinic-data/src/main/java/fr/afcepf/ad1/springpetclinic/model/PetType.java
@@ -0,0 +1,11 @@
+package fr.afcepf.ad1.springpetclinic.model;
+
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PetType extends BaseEntity {
+    private String name;
+
+}
